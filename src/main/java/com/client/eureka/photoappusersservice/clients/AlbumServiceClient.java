034136a61cb95f17f337948bca5275151f9c831a
ravindar86 <ravindar.mca@gmail.com>
2022-03-14 08:14:31 +0530

    Commit for adding Retry resilience4j component which retries for specific interval if microservice is down.

--- src/main/java/com/client/eureka/photoappusersservice/clients/AlbumServiceClient.java
+++ src/main/java/com/client/eureka/photoappusersservice/clients/AlbumServiceClient.java
@@ -2,9 +2,11 @@ package com.client.eureka.photoappusersservice.clients;
 
 import com.client.eureka.photoappusersservice.model.response.AlbumResponseModel;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -17,6 +19,7 @@ public interface AlbumServiceClient {
     Logger logger = LoggerFactory.getLogger(AlbumServiceClient.class);
 
     @GetMapping("/users/{id}/albums")
+    @Retry(name="albums-service")
     @CircuitBreaker(name="albums-service", fallbackMethod = "getAlbumsFallback")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
 
